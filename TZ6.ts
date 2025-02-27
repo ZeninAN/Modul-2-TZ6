0520f5ad1b1064c95670abf8bd40712954cd7000
ZeninAN <95536097+ZeninAN@users.noreply.github.com>
2025-02-27 17:35:01 +0400

    first commit

--- TZ6.ts
+++ TZ6.ts
@@ -1,49 +1,41 @@
-function DecoCar(constructor: Function) {
+function DecoratorCar(constructor: Function) {
     Object.seal(constructor.prototype);
 }
   
-@DecoCar
+@DecoratorCar
 class Car{
-    private _model: string;
+    private _brand: string;
     private _year : number;
-    constructor(public model: string, public year: number) {
-        this._model = model;
+    constructor(public brand: string, public year: number) {
+        this._brand = brand;
         this._year = year;
     }
 }
 
-const car = new Car("Tesla", 2021);
+const car = new Car("Lancia", 1999);
 console.log(Object.isExtensible(Object.getPrototypeOf(car)));
 
-//Декоратор метода
-function toUpperCase(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-
-    descriptor.value = function (...args: any[]) {
-        const result = originalMethod.apply(this, args);
-        if (typeof result === "string") {
-            return result.toUpperCase();
-        }
-        return result;
-    };
+function toUpperCase1(method: Function) {
+    return function (this: any, ...args: any[]) {
+        const result = method.apply(this, args);
+        return result.toUpperCase();
+    }
 }
 
 class Cars {
-    private _model: string;
+    private _brand: string;
     private _year: number;
 
-    constructor(model: string, year: number) {
-        this._model = model;
+    constructor(brand: string, year: number) {
+        this._brand = brand;
         this._year = year;
     }
 
-    @toUpperCase
+    @toUpperCase1
     getInfoCar(): string {
-        return `Model this car: ${this._model}, Year this car: ${this._year}`;
+        return `Model this car: ${this._brand}, Year this car: ${this._year}`;
     }
 }
 
-
-
-const cars = new Cars('Tesla', 2021);
+const cars = new Cars('Lancia', 1999);
 console.log(cars.getInfoCar());
